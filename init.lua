78de555dda3bad88374b1b90ce8eecba68a3af13
kowasaur <75297283+kowasaur@users.noreply.github.com>
2024-09-23 12:26:35 +1000

    copilot and relative line numbers

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -156,6 +156,9 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+-- :terminal
+vim.opt.shell = 'fish'
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
