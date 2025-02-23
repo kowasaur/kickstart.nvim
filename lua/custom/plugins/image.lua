2b048f3c65f52c8d46876c3568deab117b95aa22
kowasaur <75297283+kowasaur@users.noreply.github.com>
2025-02-23 14:50:21 +1000

    Tex
    And fix images for desktop

--- lua/custom/plugins/image.lua
+++ lua/custom/plugins/image.lua
@@ -2,6 +2,7 @@ return {
   '3rd/image.nvim',
   build = false, -- don't use rock
   opts = {
+    processor = 'magick_cli',
     integrations = {
       markdown = {
         only_render_image_at_cursor = true,
